a0a3c213a4f2616d8a6deeebb89dbfbe518ff7c5
Musahaf Ali <musahafali43@gmail.com>
2025-12-31 18:27:13 +0500

    added all codes

---
+++ class 5/practiceQuestion.js
@@ -0,0 +1,23 @@
+// Practice Question
+
+let totalMarks = 200;
+let obtMarks = 130 ;
+let atdPerc = 60;
+let bonusMarks = 20 ;
+let stAge = 18 ;
+
+// let marksPer = obtMarks / totalMarks * 100
+// obtMarks += bonusMarks
+// obtMarks  = obtMarks  + bonusMarks
+// atdPerc += 20 ;
+// console.log(marksPer >= 40);
+// console.log(stAge >= 18);
+// bonusMarks++
+// atdPerc--
+// let marksPer = obtMarks / totalMarks * 100
+// obtMarks += bonusMarks
+// obtMarks  = obtMarks  + bonusMarks
+// atdPerc += 20 ;
+// console.log(marksPer >= 40 && stAge >= 18 && atdPerc >= 75);
+
+// console.log("Student marks percentage = " , marksPer);
---
+++ class 5/script.js
@@ -0,0 +1,74 @@
+// console.log("Hello world");
+// Arithmatic Operators
+// let num1 = 4 ;
+// let num2 = 3 ;
+// console.log("The sum of num1 + num2 = " , num1 + num2);
+// console.log("The Subs of num1 - num2 = " , num1 - num2);
+// console.log("The mult of num1 * num2 = " , num1 * num2);
+// console.log("The div of num1 / num2 = " , num1 / num2);
+// console.log(num1 % num2 );
+// console.log(num1 ** num2);
+
+// Area of Square = length * width
+// let length = 3 ;
+// let width = 4 ;
+
+// let Area = length * width
+// console.log(Area);
+
+// Assignment Operators
+
+// let number = 5 ;
+// number += 5
+// console.log(number);
+// number -= 4
+// console.log(number);
+// number *= 5 
+// console.log(number);
+// number /= 6 
+// console.log(number);
+
+// let salary = 50000 ;
+// let inc = 10000
+// salary += inc
+// console.log(salary);
+
+// comparison operators 
+
+
+// console.log(4 > 8);
+// console.log(30 < 40 );
+// console.log(30 <= 30 );
+// console.log(50 >= 30 );
+// let name = "mouse"
+// console.log(name == "Laptop");
+// console.log('10' === '10');
+// let name = "Musahaf"
+// console.log(name !== "Musahaf");
+// let username = "insta.com"
+// let password = '343'
+// let sta = "active"
+// console.log(username == "insta.com" && password == "123" && statu == "active");
+// console.log(username == "facebook.com" || password == "123" || sta== "unactive");
+
+// let isStudent = false
+// console.log(!isStudent);
+
+// let count = 1 ;
+// // count++
+// // count++
+// count--
+// count--
+// console.log(count);
+
+// console.log(3*5+6);
+
+
+
+
+
+
+
+
+
+
