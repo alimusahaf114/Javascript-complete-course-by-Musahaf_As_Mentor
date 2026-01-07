5288de4bab7fcc00a01e0dd07a392006e097d68a
Musahaf Ali <musahafali43@gmail.com>
2026-01-07 17:46:24 +0500

    added values

---
+++ class 7/script.js
@@ -0,0 +1,54 @@
+// for(initialization ; condition ; update){
+// code
+// }
+// let value = 0
+// for(let i = 1 ; i <= 5 ; i++){
+//     value += i
+// }
+// console.log(value);
+// for(let i = 1 ; i <= 10 ; i++){
+//     console.log(`${i} * 3 = ${i*3}`);
+    
+// }
+
+
+//while loo
+// while(condition){
+// code
+// }
+// let i = 1;
+// while(i <= 5){
+//     console.log(i);
+//     i++;
+// }
+
+// let lives = 3;
+// while(lives >= 0){
+//     console.log("Playing...");
+//     lives--    
+// }
+// console.log("Game Over...");
+
+// let i = 11 ;
+// do{
+//     console.log("Playing");
+//     i++    
+// }while(i <= 10)
+
+
+// for(let i = 1 ; i <= 5 ; i++){
+    
+//     if(i == 3){
+//         continue;
+//     }
+//     console.log(i);
+    
+// }
+
+// for(let i = 1 ; i <= 2 ; i++){
+//     for(let j = 1 ; j <= 4 ; j++){
+//         console.log(j);
+        
+//     }
+    
+// }
