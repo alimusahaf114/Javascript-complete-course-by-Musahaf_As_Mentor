3b388b71f2b165f661a194861380452dbd88d01f
Musahaf Ali <musahafali43@gmail.com>
2026-01-25 13:07:12 +0500

    added all videos

---
+++ class 10 p2/script.js
@@ -0,0 +1,23 @@
+// console.log("Subscribe krna zaroori hai");
+// let elem = document.getElementById("heading")
+// let mySpan = document.getElementById("mySpan")
+// console.log(mySpan);
+
+// let para = document.getElementsByClassName("para")
+// console.log(para[1]);
+
+// let myelem = document.getElementsByTagName("h2")
+// console.log(myelem[2]);
+
+// let myelem = document.querySelector(".heading")
+// console.log(myelem);
+
+
+// let elem = document.querySelector("h1")
+// elem.innerText = "You are replaced and again you are changed"
+// document.querySelector("#box").textContent = "yOU ARE DIV"
+
+// document.querySelector("button").innerText = "Click"
+
+// let text = document.querySelector("h1").textContent
+// console.log(text);
