095396259ecde7ac949318bad24ec2416f9be1b4
Musahaf Ali <musahafali43@gmail.com>
2026-01-26 13:05:06 +0500

    added videos code

---
+++ class 10 p3/script.js
@@ -0,0 +1,22 @@
+// console.log("Dom");
+// document.body.style.backgroundColor = "green"
+// let isColored = false;
+// if(isColored){
+// document.querySelector("h1").style.color = "red"
+// document.querySelector("h1").style.backgroundColor = "yellow"
+// }
+
+// document.querySelector("div").style.width = "200px"
+// document.querySelector("div").style.height = "200px"
+// document.querySelector("div").style.backgroundColor = "grey"
+
+
+// document.body.innerHTML = `
+//     <p class="para"> This is para used for practice </p>
+//     <h1> This is dynamically created H1 </h1>
+// `
+
+// document.querySelector(".box").innerHTML = `
+//     <p class="para"> This is para used for practice </p>
+//     <h1> This is dynamically created H1 </h1>
+// `
